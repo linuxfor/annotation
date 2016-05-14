6452f1d73fb65eedcfc08acb324f76b001e072b7
Rainbow888 <1099634723@qq.com>
2016-05-14 11:36:15 +0800

    change

---
+++ app/src/main/java/com/qhung/annotation/MainActivity.java
@@ -0,0 +1,29 @@
+package com.qhung.annotation;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.text_view)
+    TextView textView;
+
+    @OnClick(R.id.text_view)
+     void onClick(View view) {
+        textView.setText("我被click了");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        textView.setText("我还没有被click");
+    }
+}
